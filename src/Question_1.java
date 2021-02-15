40c29741e782fe1b3de13fa7d7e60fd7e319f665
FrancisDavison <francis.nj.davison@gmail.com>
2021-02-15 18:31:33 +0000

    added code

--- src/Question_1.java
+++ src/Question_1.java
@@ -1,7 +1,44 @@
+import java.util.ArrayList;
+
 public class Question_1
 {
 	public static void main(String args[])
 	{
-		
+		ArrayList<Integer> rep = new ArrayList<Integer>();
+		/*
+		rep.add(1);
+		rep.add(0);
+		rep.add(1);
+		rep.add(0);
+		rep.add(1);
+		rep.add(0);
+		rep.add(1);
+		rep.add(0);
+		rep.add(1);
+		rep.add(0);
+		*/
+		OneMaxFitness(rep);
+	}
+	
+	public static double OneMaxFitness(ArrayList<Integer> rep)
+	{
+		double Test1=-1;
+		double Test2=-2;
+		int n=rep.size();
+		if(rep.isEmpty()==true)
+		{
+			return Test1;
+		}
+		if(rep!=null)
+		{
+			for(int i=0;i<n;i++)
+			{
+				if(rep.get(i)!=0&&rep.get(i)!=1)
+				{
+					return Test2;
+				}
+			}
+		}
+		return Test1;
 	}
 }
